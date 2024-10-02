a3c3d57ad5c42cfc4d9ff14471e984f664a301f9
nana33151 <164616449+nana33151@users.noreply.github.com>
2024-10-02 23:45:29 +0300

    Revert "Merge branch 'HACK_51_101_102'"
    This reverts commit 0e71f1c48a9c1f50ee31f29546de5a33f2d4af29, reversing
    changes made to ded79c34e8acf78d82db2b4a277485e93c63b5c8.

--- base/views.py
+++ base/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
-from .models import User, Topic, JobStatus, Job, MatchType, Match, ReviewType, Review, Message, Product
+from .models import User, Topic, JobStatus, Job, MatchType, Match, ReviewType, Review, Message
 from .forms import JobForm, UserForm, MyUserCreationForm
 import re
 
